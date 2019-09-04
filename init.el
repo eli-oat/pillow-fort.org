51b0e70e7998ab735c92f5cd28ee9570654fb7fb
Eli Mellen <eli@newstartmobile.com>
2019-09-04 16:17:38 -0400

    ...

--- init.el
+++ init.el
@@ -62,4 +62,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-done ((t (:foreground "PaleGreen" :weight normal :strike-through t))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "#5E81AC" :strike-through t)))))
