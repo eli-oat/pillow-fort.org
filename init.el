0557c8c3eea89f1b5cbdda3e74bcb1695658be2a
eli <hi@eli.li>
2019-09-03 21:11:57 -0400

    starting a new

---
+++ init.el
@@ -0,0 +1,46 @@
+;;; init.el --- Emacs init file
+;;  Author: Eli Mellen
+;;; Commentary:
+;;; A lightweight Emacs config containing only the stuff I know how to use!
+;;; Code:
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6
+      file-name-handler-alist-original file-name-handler-alist
+      file-name-handler-alist nil
+      site-run-file nil)
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold 20000000
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist file-name-handler-alist-original)
+            (makunbound 'file-name-handler-alist-original)))
+
+(add-hook 'minibuffer-setup-hook (lambda () (setq gc-cons-threshold 40000000)))
+(add-hook 'minibuffer-exit-hook (lambda ()
+                                  (garbage-collect)
+                                  (setq gc-cons-threshold 20000000)))
+
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; workaround bug in Emacs 26.2
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+;; Setting up the package manager. Install if missing.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t))
+
+;; Load main config file "./pillow-fort.org"
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/pillow-fort.org"))
+
+(provide 'init)
+;;; init.el ends here
